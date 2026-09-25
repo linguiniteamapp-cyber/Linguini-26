ba09611053e677d1a7e0128ea1400153613dc3c5
Mohanad Ahmed <ahmdmohanad28@gmail.com>
2026-09-25 22:09:30 +0300

    feat: add main entry point with Firebase initialization and SplashPage root

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:linguini_app/homepage.dart';
-import 'otp_temp.dart';
-import 'package:linguini_app/choose_account.dart';
-import 'package:linguini_app/login.dart';
-import 'package:linguini_app/profile_setup_1.dart';
-import 'package:linguini_app/basket.dart';
-import 'splash_page.dart';
-import 'package:linguini_app/chat.dart';
-import 'package:linguini_app/chef_profile_page.dart';
-import 'package:linguini_app/home/home_root_page.dart';
-import 'package:linguini_app/welcome/wepage1.dart';
-import 'package:linguini_app/Review.dart';
-import 'package:linguini_app/forgetPassword.dart';
-import 'package:linguini_app/order.dart';
-import 'package:linguini_app/active_order.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,9 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  SplashPage(),
+      home: SplashPage(),
     );
   }
 }
